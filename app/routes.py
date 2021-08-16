8c7915befc95d147a09aef586bfb9b51ff3ec404
Mihai Racoreanu <racoreanu@cardiff.ac.uk>
2021-08-16 15:50:55 +0100

    Users are asked for username and room number

--- app/routes.py
+++ app/routes.py
@@ -3,5 +3,15 @@ from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html')
+    else:
+        return redirect(url_for('home'))
