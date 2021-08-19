e38d2350f4a1f2987e01fc4515239920a8195f9c
Mihai Racoreanu <racoreanu@cardiff.ac.uk>
2021-08-19 17:14:33 +0100

    Refactored Code, Better routing, POST and GET usage

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_socketio import join_room
+from flask_socketio import join_room, leave_room
 from app import app, socketio
 import random
 import string
@@ -13,30 +13,35 @@ def index():
     return render_template('index.html')
 
 @app.route('/home', methods=['GET', 'POST'])
-def home():
-    username = request.args.get('username')
+@app.route('/home/<string:username>', methods=['GET', 'POST'])
+def home(username=None):
+    if request.method == "POST":
+        username = request.form.get('username')
     return render_template('home.html', username=username)
 
-@app.route('/new_lobby/<string:username>')
-def new_lobby(username):
-    letters = string.ascii_uppercase
-    room = ''.join(random.choice(letters) for i in range(4))
-    game_rooms[room] = {}
-    game_rooms[room].setdefault('users', []).append(username)
-    return render_template('lobby.html', room=room, username=username, users=game_rooms[room]['users'], host=True)
+@app.route('/new_lobby', methods=['GET', 'POST'])
+def new_lobby():
+    if request.method == "POST":
+        username = request.form.get('username')
+        letters = string.ascii_uppercase
+        room = ''.join(random.choice(letters) for i in range(4))
+        game_rooms[room] = {}
+        game_rooms[room].setdefault('users', []).append(username)
+    return redirect(url_for('game', room=room, username=username, host=int(True)))
 
-@app.route('/leave_room')
-def leave_room():
-    pass
-
-@app.route('/join/<string:username>')
-def join(username):
-    room = request.args.get('room')
-    if room:
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if request.method == "POST":
+        username = request.form.get('username')
+        room = request.form.get('room')
+        if not room:
+            return redirect(url_for('home', username=username))
         game_rooms[room]['users'].append(username)
-        return render_template('lobby.html', room=room, username=username, users=json.dumps(game_rooms[room]['users']))
-    else:
-        return redirect(url_for('home', username=username))
+    return redirect(url_for('game', room=room, username=username, host=int(False)))
+
+@app.route('/game/<string:room>/<string:username>/<int:host>')
+def game(room, username, host):
+    return render_template('lobby.html', room=room, username=username, users=game_rooms[room]['users'], host=host)
 
 @socketio.on('join_room')
 def handle_join_room_event(data):
@@ -47,7 +52,7 @@ def handle_join_room_event(data):
 @socketio.on('leave_room')
 def handle_leave_room_event(data):
     app.logger.info("{} has left the room {}".format(data['username'], data['room']))
-    leave_room()
+    leave_room(data['room'])
     user_list = game_rooms[data['room']]['users']
     user_list.remove(data['username'])
     data['users'] = user_list
