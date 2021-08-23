c8f3b0aae061ba804da2d7a2d49a9e43fec5aaa9
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2021-08-23 19:44:20 +0100

    More Cosmetics

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PjALA07lE1iFIP3heFAq'
-    IP_ADDRESS = "192.168.0.12"
+    IP_ADDRESS = "192.168.0.14"
