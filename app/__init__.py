5c0c61e66eec2b9643f44c0592e868daeebb0768
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2021-09-07 17:41:06 +0100

    Haida si cu CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from config import Config
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
-socketio = SocketIO(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 from app import routes, models
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Config(object):
     ADDRESS = "127.0.0.1:5000"
+    CORS_HEADERS = 'Content-Type'
