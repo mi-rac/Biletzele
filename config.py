6c47826bd2e02c29371ce972d4d2b6e3fdefb3d4
Mihai Racoreanu <mihai.racoreanu@gmail.com>
2021-09-07 12:20:48 +0100

    Procfile

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'PjALA07lE1iFIP3heFAq'
-    IP_ADDRESS = "192.168.0.14"
+    IP_ADDRESS = "127.0.0.1"
